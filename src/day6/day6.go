d8eec0887948c48303d97423361bf79f70bf6d61
jbyers44 <jbyers44@gmail.com>
2022-12-06 09:20:21 -0700

    Day 6 solved, added various slice utils

---
+++ src/day6/day6.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"adventofcode2022/utils"
+	"bufio"
+	"fmt"
+)
+
+func main() {
+	part1()
+	part2()
+}
+
+func part1() {
+	file := utils.OpenFile("../../input/day6.txt")
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+
+	line := scanner.Text()
+
+	_, index := utils.UniqueSlice([]rune(line), 4)
+
+	fmt.Printf("%v\n", index+1)
+}
+
+func part2() {
+	file := utils.OpenFile("../../input/day6.txt")
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+
+	line := scanner.Text()
+
+	_, index := utils.UniqueSlice([]rune(line), 14)
+
+	fmt.Printf("%v\n", index+1)
+}
--- utils/parse.go
+++ utils/parse.go
@@ -9,3 +9,32 @@ func Contains(list []rune, char rune) bool {
 
 	return false
 }
+
+func IsUnique(buf []rune) bool {
+	a := []rune{}
+
+	for _, c := range buf {
+		if Contains(a, c) {
+			return false
+		}
+		a = append(a, c)
+	}
+
+	return true
+}
+
+func UniqueSlice(data []rune, len int) (unique []rune, index int) {
+	buf := make([]rune, len)
+	index = -1
+
+	for i, c := range data {
+		buf = append(buf, c)
+		buf = buf[1:]
+		if i > len && IsUnique(buf) {
+			index = i
+			break
+		}
+	}
+
+	return
+}
